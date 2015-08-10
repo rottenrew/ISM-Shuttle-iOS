a0c1f0dbec19bbaf7dfb714570f4c75b1bdebf4a
Shubham Chauhan <shubham.cse@outlook.com>
2015-08-11 00:53:32 +0530

    Basic UI
    Complete Storyboard UI for GPS tracking app using WebKit

--- ISM Shuttle/AppDelegate.swift
+++ ISM Shuttle/AppDelegate.swift
@@ -3,7 +3,6 @@
 //  ISM Shuttle
 //
 //  Created by Shubham Chauhan on 10/08/15.
-//  Copyright (c) 2015 Shubham Chauhan. All rights reserved.
 //
 
 import UIKit
---
+++ ISM Shuttle/MapViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MapViewController.swift
+//  ISM Shuttle
+//
+//  Created by Shubham Chauhan on 10/08/15.
+//  Copyright (c) 2015 Shubham Chauhan. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let initialLocation = CLLocation(latitude: 23.81, longitude: 86.44)
+        centerMapOnLocation(initialLocation)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let regionRadius: CLLocationDistance = 1000
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
