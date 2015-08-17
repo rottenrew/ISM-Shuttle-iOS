49f29c9b716d23768452126999addb7d6045cf2c
Shubham Chauhan <shubham.cse@outlook.com>
2015-08-18 02:47:58 +0530

    Tracking
    Tracking system implemented.

--- ISM Shuttle/AppDelegate.swift
+++ ISM Shuttle/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Foundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- ISM Shuttle/MapViewController.swift
+++ ISM Shuttle/MapViewController.swift
@@ -12,11 +12,26 @@ import MapKit
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    var bytes: NSMutableData?
+    var bytes_final: NSMutableData?
+
+
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        var route: MKOverlay
+        self.mapView.showsUserLocation = true
         let initialLocation = CLLocation(latitude: 23.81, longitude: 86.44)
         centerMapOnLocation(initialLocation)
+        
         // Do any additional setup after loading the view.
+        
+        trackBus()
+        var busLocation = MKPointAnnotation()
+        var lat  = 23.81
+        var longi = 86.44
+        busLocation.coordinate = CLLocationCoordinate2D(latitude:lat, longitude:longi)
+        mapView.addAnnotation(busLocation)
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,8 +41,34 @@ class MapViewController: UIViewController {
     
     func centerMapOnLocation(location: CLLocation) {
         let regionRadius: CLLocationDistance = 1000
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,regionRadius * 2.0, regionRadius * 2.0)
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,regionRadius * 1.5, regionRadius * 1.5)
         mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func trackBus(){
+        let request = NSURLRequest(URL: NSURL(string: "http://shuttletracker.hostei.com/?bus=1&latest=1")!)
+        let loader = NSURLConnection(request: request, delegate: self, startImmediately: true)
+
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData conData: NSData!) {
+        //if conData = '}'
+        self.bytes?.appendData(conData)
+    }
+    
+    func connection(didReceiveResponse: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
+        self.bytes = NSMutableData()
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        
+        // we serialize our bytes back to the original JSON structure
+        //println("Check this : \(bytes)")
+        //bytes?.resetBytesInRange(NSMakeRange(87, 150))
+        //println("Check this again : \(bytes)")
+        
+        var jsonResult : Dictionary = NSJSONSerialization.JSONObjectWithData(self.bytes!, options: NSJSONReadingOptions.MutableContainers, error: nil) as! Dictionary<String, AnyObject>
+        
     }
 
     /*
