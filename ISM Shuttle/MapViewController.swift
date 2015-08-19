991cd7157bbd5e112eb97497b52e64a186239171
Shubham Chauhan <shubham.cse@outlook.com>
2015-08-20 01:23:45 +0530

    Track
    Link updated. Need to fix Lat/Long conversions

--- ISM Shuttle/MapViewController.swift
+++ ISM Shuttle/MapViewController.swift
@@ -3,18 +3,15 @@
 //  ISM Shuttle
 //
 //  Created by Shubham Chauhan on 10/08/15.
-//  Copyright (c) 2015 Shubham Chauhan. All rights reserved.
+//  Copyright (c) 2015 Ayush AGRAwal. All rights reserved.
 //
 
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, NSURLConnectionDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
-    var bytes: NSMutableData?
-    var bytes_final: NSMutableData?
-
 
     override func viewDidLoad() {
         
@@ -23,15 +20,21 @@ class MapViewController: UIViewController {
         self.mapView.showsUserLocation = true
         let initialLocation = CLLocation(latitude: 23.81, longitude: 86.44)
         centerMapOnLocation(initialLocation)
-        
+
         // Do any additional setup after loading the view.
         
-        trackBus()
+        let json = JSON.fromURL("http://shuttletracker.zz.mu/?bus=1&latest=1")
+        
+        let lat:Double = (json["latitude"].toString() as NSString).doubleValue
+        let lon:Double = (json["longitude"].toString() as NSString).doubleValue
+        
+        println("Latitude : \(lat/100) , Longitude : \(lon/100)")
+        
         var busLocation = MKPointAnnotation()
-        var lat  = 23.81
-        var longi = 86.44
-        busLocation.coordinate = CLLocationCoordinate2D(latitude:lat, longitude:longi)
+        busLocation.coordinate = CLLocationCoordinate2D(latitude:lat/100, longitude:lon/100)
+        
         mapView.addAnnotation(busLocation)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,32 +47,7 @@ class MapViewController: UIViewController {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,regionRadius * 1.5, regionRadius * 1.5)
         mapView.setRegion(coordinateRegion, animated: true)
     }
-    
-    func trackBus(){
-        let request = NSURLRequest(URL: NSURL(string: "http://shuttletracker.hostei.com/?bus=1&latest=1")!)
-        let loader = NSURLConnection(request: request, delegate: self, startImmediately: true)
 
-    }
-    
-    func connection(connection: NSURLConnection!, didReceiveData conData: NSData!) {
-        //if conData = '}'
-        self.bytes?.appendData(conData)
-    }
-    
-    func connection(didReceiveResponse: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
-        self.bytes = NSMutableData()
-    }
-    
-    func connectionDidFinishLoading(connection: NSURLConnection!) {
-        
-        // we serialize our bytes back to the original JSON structure
-        //println("Check this : \(bytes)")
-        //bytes?.resetBytesInRange(NSMakeRange(87, 150))
-        //println("Check this again : \(bytes)")
-        
-        var jsonResult : Dictionary = NSJSONSerialization.JSONObjectWithData(self.bytes!, options: NSJSONReadingOptions.MutableContainers, error: nil) as! Dictionary<String, AnyObject>
-        
-    }
 
     /*
     // MARK: - Navigation
